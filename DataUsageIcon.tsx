97cb283ffcc184c7405ad2fa4f306410d25c777e
devhima <ibrahhm212@gmail.com>
2025-10-05 04:20:05 +0300

    Last Sync: 2025-10-05 04:20 (Mobile)

--- DataUsageIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const DataUsageIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.042A5.25 5.25 0 0112 12m0 0A5.25 5.25 0 0115.712 8.958m-3.712 6.084L12 12m0 0l-3.712-3.042m3.712 6.084V18m0-12V6m6.084 6.288h3.042m-12.084 0H6M12 21a9 9 0 110-18 9 9 0 010 18z" />
-  </svg>
-);
